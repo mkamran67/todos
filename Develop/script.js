9d39a3a00c71a4cd4bf7ec83a2992f1b2865ab96
Muhammad Kamran <mkamran1967@gmail.com>
2020-08-14 00:59:17 -0700

    init

---
+++ Develop/script.js
@@ -0,0 +1,7 @@
+// 1. On Open -> Show Current Day -> Reload LocalStorage Data
+// 2. On Scroll down show day planner.
+/* 3. 
+    -- Day Planner 📃
+    [] Blocks of Time -> Color coded for time (Past,Present, Future)
+    [] TimeBlock.onClick -> Ability to input an event -> Save to LocalStorage     
+*/
